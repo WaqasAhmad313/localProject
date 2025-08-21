59307cb9752788cae3e22076a3de6fd6baded95a
Waqas Ahmad <waqasfarooq2277@gmail.com>
2025-08-21 18:44:15 +0500

    another function added

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const first =(data)=>{
 console.log(`Here's the data you send ${data}`)
 }
 
-first(1234);
+first(1000);
 
 const secondfunction = (a,b)=>{
     return a + b
 }
-secondfunction(20,40);
+secondfunction(100,500);
+
+function thirdfunction (a,b){
+    return a * b
+}
+thirdfunction(10, 20);
